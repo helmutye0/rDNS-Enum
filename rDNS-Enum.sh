7f5b04718404acb0d8d6186ba223a700836ef5ee
helmutye0 <86020593+helmutye0@users.noreply.github.com>
2023-02-22 15:28:09 -0500

    Update rDNS-Enum.sh

--- rDNS-Enum.sh
+++ rDNS-Enum.sh
@@ -1,28 +1,42 @@
 #!/bin/bash
 
-ip24=1
-cidr="10.1"
+cidr=10
+ip16=0
 
-while [ $ip24 -le 255 ]
+while [ $ip16 -le 255 ]
 do
 
-        echo "$cidr.$ip24.0/24"
-        interface=1
+	ip24=0
 
-        while [ $interface -le 255 ]
-        do
-                host=""
+	while [ $ip24 -le 255 ]
+	do
+
+        echo "$cidr.$ip16.$ip24.0/24"
+        interface=0
+
+			while [ $interface -le 255 ]
+			do
+                
+				host=""
                 ip=$cidr.$ip24.$interface
                 host=$(nslookup $ip | grep msufcu)
                 
                 if [ "$host" ]
                 then
-                        host=$(echo $host | cut -d "=" -f 2 | cut -d " " -f 2 | cut -d "." -f 1,2,3)
-                        echo "$ip | $host" >> dns-hosts.txt
+                
+					host=$(echo $host | cut -d "=" -f 2 | cut -d " " -f 2 | cut -d "." -f 1,2,3)
+                    echo "$ip | $host" >> dns-hosts.txt
+					
                 fi
-        ((interface++))
-        done
+				
+			((interface++))
+			
+			done
 
         ((ip24++))
+		
+	done
+	
+((ip16++))
 
 done
